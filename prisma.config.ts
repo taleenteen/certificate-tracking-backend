5cfb87a4936c0af1451c9ba9039f731e4a6cbaa5
taleenteen <teenteenza1@hotmail.com>
2026-07-02 05:45:28 +0700

    prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   migrations: {
-    seed: 'node -r dotenv/config -r ts-node/register prisma/seed.ts',
+    seed:
+      'node -r dotenv/config -r ts-node/register -e "require(\'./prisma/seed.ts\')"',
   },
   datasource: {
     url: process.env.DATABASE_URL ?? '',
